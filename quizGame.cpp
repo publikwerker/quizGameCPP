29dbf68967749c7e43dd29d1a1c74e0122ced557
Jason Hoffman <hello@publikwerks.com>
2020-08-30 19:59:30 -0700

    first commit

---
+++ quizGame.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cctype>
+#include<cstring>
+
+using namespace std;
+
+// **************** //
+//   Publikwerker   //
+
+
+int main() {
+	cout << "This is the question.";
+
+
+	return 0;
+}
